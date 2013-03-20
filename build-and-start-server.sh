436bbc38c4ea0029da20752fb64b0aa668c37298
Mikael Vik <mikael.vik@gmail.com>
2013-03-20 22:17:28 +0100

    Faster jetty shutdown. Faster build.

--- build-and-start-server.sh
+++ build-and-start-server.sh
@@ -1,1 +1,11 @@
-mvn package && java -jar target/mongodb-tutorial-1.0-SNAPSHOT.jar server mongodb-workshop-local.yml
+#!/bin/sh
+
+# remove build catalog
+rm -rf target/
+
+# build package without tests
+mvn -DskipTests package 
+
+# start the server with configuration (yml)
+java -jar target/mongodb-tutorial-1.0-SNAPSHOT.jar server mongodb-workshop-local.yml
+
